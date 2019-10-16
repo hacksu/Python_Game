a6d98f05a531670085cd69983f992a263c8e6efa
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2019-10-15 20:26:25 -0400

    Fixed the comment indents

--- game.py
+++ game.py
@@ -35,18 +35,18 @@ while turns > 0:
     # for every character in secret_word
     for char in word:
 
-    # see if the character is in the players guess
+        # see if the character is in the players guess
         if char in guesses:
 
-        # print then out the character
+            # print then out the character
             print (char),
 
         else:
 
-        # if not found, print a dash
+            # if not found, print a dash
             print ("_"),
 
-        # and increase the failed counter with one
+            # and increase the failed counter with one
             failed += 1
 
     # if failed is equal to zero
@@ -55,7 +55,7 @@ while turns > 0:
     if failed == 0:
         print ("You won")
 
-    # exit the script
+        # exit the script
         break
 
     print
@@ -69,17 +69,17 @@ while turns > 0:
     # if the guess is not found in the secret word
     if guess not in word:
 
-     # turns counter decreases with 1 (now 9)
+        # turns counter decreases with 1 (now 9)
         turns -= 1
 
-    # print wrong
+        # print wrong
         print ("Wrong")
 
-    # how many turns are left
+        # how many turns are left
         print ("You have", + turns, 'more guesses')
 
-    # if the turns are equal to zero
+        # if the turns are equal to zero
         if turns == 0:
 
-        # print "You Loose"
+            # print "You Loose"
             print ("You Loose")
